259676e92db3965c7c177d3212567b2728ea4273
Juaneth <euanwalker2008@gmail.com>
2022-04-24 05:54:29 +0100

    added axios, added basic mc server api stuff and simple logs

---
+++ index.js
@@ -0,0 +1,38 @@
+function getVersion(type, version) {
+    if (type == "vanilla") {
+        const axios = require('axios');
+
+        axios.get('https://launchermeta.mojang.com/mc/game/version_manifest.json').then(response => {
+                const versions = response.data.versions;
+                versions.forEach(element => {
+                    if (element.type == "release") {
+                        if (element.id == version) {
+                            axios.get(element.url).then(response => {
+                                    let download = response.data.downloads.server.url;
+
+                                    return download;
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                });
+                        }
+                    }
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
+
+
+
+console.log(getVersion("vanilla", "1.14.4"));
+
+function log(type, content) {
+    if (type == "error") {
+        console.log("\x1b[31m%s\x1b[0m", `${type.toUpperCase()} || ${content}`);
+    } else if (type == "success") {
+        console.log("\x1b[32m%s\x1b[0m", `${type.toUpperCase()} || ${content}`);
+    }
+}
