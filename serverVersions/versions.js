7b67e8faaacf138873cbf527e49773f8bbdaebfe
Juaneth <euanwalker2008@gmail.com>
2022-05-21 00:23:49 +0100

    farrdddd

--- serverVersions/versions.js
+++ serverVersions/versions.js
@@ -77,6 +77,7 @@ module.exports = {
                                     "1.2.3",
                                     "1.2.4",
                                 ]
+
                                 if (!historicVersions.includes(element.id)) {
                                     allVersions.push(element);
                                 }
